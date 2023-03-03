c8c37911b330f1556e4b6d5235ac2fe6bcb4c913
yarek <yarek2003@mail.ru>
2023-03-04 01:57:16 +0300

    Done!

---
+++ src/pro/sky/java/course1/homework13/Author.java
@@ -0,0 +1,25 @@
+package pro.sky.java.course1.homework13;
+
+public class Author {
+    String firstName;
+    String lastName;
+    public Author (String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
---
+++ src/pro/sky/java/course1/homework13/Main.java
@@ -0,0 +1,17 @@
+package pro.sky.java.course1.homework13;
+
+public class Main {
+    public static void main(String[] args) {
+    Author dostoevsky = new Author("Федор", "Достоевский");
+    Author golding = new Author("Уильям", "Голдинг");
+    Book idiot = new Book("Идиот", dostoevsky, 1998);
+    Book lordOfTheFlies = new Book("Повелитель мух", golding, 2012);
+        System.out.println(idiot.getTitle() +  " " + dostoevsky.getFirstName() +
+                " " + dostoevsky.getLastName() +  " " + idiot.getYear());
+        System.out.println(lordOfTheFlies.getTitle() + " " + golding.getFirstName() +
+                " " + golding.getLastName()  + " " +  lordOfTheFlies.getYear());
+    idiot.setYear(2000);
+        System.out.println("Идиот, Издание " + idiot.year + " года");
+    }
+
+}
